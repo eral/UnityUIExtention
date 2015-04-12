c564b0fa6de03c09c1d24f72ee41194460747142
eral.r.l <eral.r.l+git@gmail.com>
2015-04-13 00:12:36 +0900

    :heavy_plus_sign:ColorGroup:First commit

---
+++ Assets/App/Product/ColorGroup/ColorGroup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ColorGroup : Graphic {
+
+	private CanvasRenderer[] m_CanvasRenderers = null;
+	private CanvasRenderer[] canvasRenderers {get{
+		if ((m_CanvasRenderers == null) || (m_CanvasRenderers.Length == 0)) {
+			m_CanvasRenderers = GetComponentsInChildren<CanvasRenderer>();
+		}
+		return m_CanvasRenderers;
+	}}
+
+	protected virtual void Update() {
+		var color = canvasRenderers[0].GetColor();
+		foreach (var canvasRenderer in canvasRenderers.Skip(1)) {
+			canvasRenderer.SetColor(color);
+		}
+	}
+
+	protected virtual void OnTransformChildrenChanged() {
+		m_CanvasRenderers = null;
+	}
+
+	protected override void OnFillVBO(List<UIVertex> vbo) {
+	}
+
+	public override bool Raycast(Vector2 sp, Camera eventCamera) {
+		return false;
+	}
+
+}
---
+++ Assets/App/Product/ColorGroup/Editor/ColorGroupEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColorGroup))]
+public class ColorGroupEditor : Editor {
+
+	public override void OnInspectorGUI() {
+	}
+
+}
