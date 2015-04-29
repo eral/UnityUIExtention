8b9238fba51d480aa8fcf8adca9a9bfe9ff5207b
eral.r.l <eral.r.l+git@gmail.com>
2015-04-29 19:40:14 +0900

    :heavy_plus_sign:Teapot:First commit

---
+++ Assets/App/Product/Teapot/Editor/TeapotEditor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(Teapot), true)]
+public class TeapotEditor : GraphicEditor {
+
+	SerializedProperty m_Sprite;
+	GUIContent m_SpriteContent;
+
+	protected override void OnEnable() {
+		base.OnEnable();
+
+		m_SpriteContent			= new GUIContent("Source Image");
+
+		m_Sprite		= serializedObject.FindProperty("m_Sprite");
+	}
+
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(m_Sprite, m_SpriteContent);
+		AppearanceControlsGUI();
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	public override bool HasPreviewGUI() {
+		return true;
+	}
+
+	public override void OnPreviewGUI(Rect rect, GUIStyle background) {
+		var Teapot = target as Teapot;
+		var sprite = Teapot.sprite;
+		if (sprite == null) {
+			return;
+		}
+
+		//UnityEditor.UI.SpriteDrawUtility.DrawSprite(sprite, rect, Teapot.canvasRenderer.GetColor());
+	}
+
+	public override string GetInfoString() {
+		var Teapot = target as Teapot;
+		var sprite = Teapot.sprite;
+
+		var x = ((sprite != null)? Mathf.RoundToInt(sprite.rect.width): 0);
+		var y = ((sprite != null)? Mathf.RoundToInt(sprite.rect.height): 0);
+
+		return string.Format("Teapot Size: {0}x{1}", x, y);
+	}
+}
