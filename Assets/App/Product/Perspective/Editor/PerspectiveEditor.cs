5c52b51a3e3d7976e6f1db8cb24618c85686fe0f
eral <eral.r.l+github.com@gmail.com>
2015-11-29 04:01:42 +0900

    :heavy_plus_sign:Perspective:First commit
    初版

---
+++ Assets/App/Product/Perspective/Editor/PerspectiveEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(Perspective), true)]
+public class PerspectiveEditor : GraphicEditor {
+
+	SerializedProperty m_FieldOfView;
+	GUIContent m_FieldOfViewContent;
+
+	protected override void OnEnable() {
+		base.OnEnable();
+
+		m_FieldOfViewContent	= new GUIContent("Field of View");
+
+		m_FieldOfView	= serializedObject.FindProperty("m_FieldOfView");
+	}
+
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(m_FieldOfView, m_FieldOfViewContent);
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
